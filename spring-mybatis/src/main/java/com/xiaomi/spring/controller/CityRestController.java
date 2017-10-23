46eef75ddd8bb3ef4c8205ce77a535225a90a60b
xiaomi <1205938863@qq.com>
2017-10-23 22:09:29 +0800

    springboot 与mybatis结合

---
+++ spring-mybatis/src/main/java/com/xiaomi/spring/controller/CityRestController.java
@@ -0,0 +1,27 @@
+package com.xiaomi.spring.controller;
+
+
+import com.xiaomi.spring.domain.City;
+import com.xiaomi.spring.service.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by bysocket on 07/02/2017.
+ */
+@RestController
+public class CityRestController {
+
+    @Autowired
+    private CityService cityService;
+
+    @RequestMapping(value = "/api/city", method = RequestMethod.GET)
+    public City findOneCity(@RequestParam(value = "cityName", required = true) String cityName) {
+        City cityByName = cityService.findCityByName(cityName);
+        return cityByName;
+    }
+
+}
